b527178fb6cd6675e80c6e31de69e59d8955aac3
Muhammad <i3acksp4ce@gmail.com>
2022-05-28 04:20:07 +0700

    chore: fix lint ts filetype

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,26 +2,22 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: [
-        '~/assets/css/tailwind.css',
-        '@/assets/less/ant-design.less',
+  css: ['~/assets/css/tailwind.css', '@/assets/less/ant-design.less'],
+
+  build: {
+    postcss: {
+      postcssOptions: require('./postcss.config.js'),
+    },
+  },
+
+  components: {
+    dirs: [
+      '~/components',
+      {
+        path: '~/global-components',
+        extensions: ['vue'],
+        prefix: 'C',
+      },
     ],
-
-    build: {
-        postcss: {
-            postcssOptions: require('./postcss.config.js'),
-        },
-    },
-
-    components: {
-        dirs: [
-            '~/components',
-            {
-                path: '~/global-components',
-                extensions: ['vue'],
-                prefix: 'C'
-            }
-        ]
-    },
-
+  },
 })
